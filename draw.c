5e0a71b32d087b77088f892d2a8739bfc7186ec2
hadufer debian11 <dufer.hassan@gmail.com>
2021-10-04 17:46:48 -0400

    graphical patch to improve precision of drawing

--- draw.c
+++ draw.c
@@ -6,7 +6,7 @@
 /*   By: hadufer <hadufer@student.42nice.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/04 15:19:57 by hadufer           #+#    #+#             */
-/*   Updated: 2021/10/04 17:36:44 by hadufer          ###   ########.fr       */
+/*   Updated: 2021/10/04 17:45:19 by hadufer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,11 +28,11 @@ int	matrix_draw(t_data *data, t_matrix *mat)
 		{
 			if (i != 0)
 			{
-				vec_iso1 = new_vec2(((j + i - 1) * FACTOR) * cos(0.8), ((j + i) * FACTOR) * sin(0.8) - mat->matrix[i][j]);
-				vec_iso2 = new_vec2(((j + i) * FACTOR) * cos(0.8), ((j + i) * FACTOR) * sin(0.8) - mat->matrix[i][j]);
+				vec_iso1 = new_vec2(((j - i - 1) * FACTOR) * cos(0.8), ((j + i) * FACTOR) * sin(0.8) - mat->matrix[i][j]);
+				vec_iso2 = new_vec2(((j - i) * FACTOR) * cos(0.8), ((j + i) * FACTOR) * sin(0.8) - mat->matrix[i][j]);
 				vec_iso1 = new_vec2((vec_iso1.x + WIDTH) / 2 , (vec_iso1.y + HEIGHT ) / 2 );
 				vec_iso2 = new_vec2((vec_iso2.x + WIDTH) / 2 , (vec_iso2.y + HEIGHT ) / 2 );
-				draw_line(data, vec_iso1, vec_iso2, 0x00FF00);
+				draw_line(data, vec_iso1, vec_iso2, 0xFF0000);
 			}
 			if (j < mat->w_matrix)
 			{
