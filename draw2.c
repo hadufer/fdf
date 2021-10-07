841878424132bb4a1a7e73e84762e2fbce04d814
hadufer debian11 <dufer.hassan@gmail.com>
2021-10-07 12:52:58 -0400

    work on color

--- draw2.c
+++ draw2.c
@@ -6,7 +6,7 @@
 /*   By: hadufer <hadufer@student.42nice.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/07 07:57:25 by hadufer           #+#    #+#             */
-/*   Updated: 2021/10/07 12:01:50 by hadufer          ###   ########.fr       */
+/*   Updated: 2021/10/07 12:50:52 by hadufer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,8 @@ void	draw_horizontal_iso(t_data *data, int i, int j)
 		vec_base_a = new_vec3(j, i, data->mat->matrix[i][j]);
 		vec_base_b = new_vec3(j + 1, i, data->mat->matrix[i][j + 1]);
 		//ZOOM
-		vec_base_a = new_vec3(vec_base_a.x * data->zoom, vec_base_a.y * data->zoom, vec_base_a.z * data->zoom);
-		vec_base_b = new_vec3(vec_base_b.x * data->zoom, vec_base_b.y * data->zoom, vec_base_b.z * data->zoom);
+		vec_base_a = new_vec3(vec_base_a.x * data->zoom, vec_base_a.y * data->zoom, vec_base_a.z * (0.10 * data->zoom));
+		vec_base_b = new_vec3(vec_base_b.x * data->zoom, vec_base_b.y * data->zoom, vec_base_b.z * (0.10 * data->zoom));
 		//ISO
 		vec2_a = new_vec2((vec_base_a.x - vec_base_a.y) * cos(ANGLE), (vec_base_a.x + vec_base_a.y) * sin(ANGLE) - vec_base_a.z);
 		vec2_b = new_vec2((vec_base_b.x - vec_base_b.y) * cos(ANGLE), (vec_base_b.x + vec_base_b.y) * sin(ANGLE) - vec_base_b.z);
@@ -37,7 +37,7 @@ void	draw_horizontal_iso(t_data *data, int i, int j)
 		//OFFSET
 		vec2_a = new_vec2(vec2_a.x + data->offset_x, vec2_a.y + data->offset_y);
 		vec2_b = new_vec2(vec2_b.x + data->offset_x, vec2_b.y + data->offset_y);
-		draw_line(data, vec2_a, vec2_b, create_trgb(0, 125 -(int)(round(vec_base_a.z/data->draw_max_num) * 255), 125 -(int)(round(vec_base_a.z/data->draw_max_num)* 255), 125 -(int)(round(vec_base_a.z/data->draw_max_num)* 255)));
+		draw_line(data, vec2_a, vec2_b, create_trgb(0, 255, 255 - (int)(round(data->mat->matrix[i][j]/data->draw_max_num) * 255), 255 - (int)(round(data->mat->matrix[i][j]/data->draw_max_num) * 255)));
 	}
 }
 
@@ -54,8 +54,8 @@ void	draw_vertical_iso(t_data *data, int i, int j)
 		vec_base_a = new_vec3(j, i, data->mat->matrix[i][j]);
 		vec_base_b = new_vec3(j, i + 1, data->mat->matrix[i + 1][j]);
 		//ZOOM
-		vec_base_a = new_vec3(vec_base_a.x * data->zoom, vec_base_a.y * data->zoom, vec_base_a.z * data->zoom);
-		vec_base_b = new_vec3(vec_base_b.x * data->zoom, vec_base_b.y * data->zoom, vec_base_b.z * data->zoom);
+		vec_base_a = new_vec3(vec_base_a.x * data->zoom, vec_base_a.y * data->zoom, vec_base_a.z * (0.10 * data->zoom));
+		vec_base_b = new_vec3(vec_base_b.x * data->zoom, vec_base_b.y * data->zoom, vec_base_b.z * (0.10 * data->zoom));
 		//ISO
 		vec2_a = new_vec2((vec_base_a.x - vec_base_a.y) * cos(ANGLE), (vec_base_a.x + vec_base_a.y) * sin(ANGLE) - vec_base_a.z);
 		vec2_b = new_vec2((vec_base_b.x - vec_base_b.y) * cos(ANGLE), (vec_base_b.x + vec_base_b.y) * sin(ANGLE) - vec_base_b.z);
@@ -65,7 +65,7 @@ void	draw_vertical_iso(t_data *data, int i, int j)
 		//OFFSET
 		vec2_a = new_vec2(vec2_a.x + data->offset_x, vec2_a.y + data->offset_y);
 		vec2_b = new_vec2(vec2_b.x + data->offset_x, vec2_b.y + data->offset_y);
-		draw_line(data, vec2_a, vec2_b, create_trgb(0, 125 -(int)(round(vec_base_a.z/data->draw_max_num) * 255), 125 -(int)(round(vec_base_a.z/data->draw_max_num)* 255), 125 -(int)(round(vec_base_a.z/data->draw_max_num)* 255)));
+		draw_line(data, vec2_a, vec2_b, create_trgb(0, 255, 255 - (int)(round(data->mat->matrix[i][j]/data->draw_max_num) * 255), 255 - (int)(round(data->mat->matrix[i][j]/data->draw_max_num) * 255)));
 	}
 }
 
